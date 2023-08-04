88d9c63cf61dd7d822fe6df39f60079269ff1eda
CHAIDARALI03 <chaidarali260@gmail.com>
2023-08-04 11:15:35 +0700

    tes

---
+++ coba/test.py
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <title>Tugas Pertemuan 2</title>
+    </head>
+    <body>
+        <h1>Belajar Tag Gambar</h1>
+        <img src="singa.JPG" alt="singa" style="width:200px;height:150px;">
+    </body>
+</html>
+
+<html>
+    <body>
+        <h1>Belajar Tag Audio</h1>
+        <audio controls autoplay loop>
+            <source src="Taste My Sin.mp3" type="audio/mp3">
+        </audio>
+    </body>
+</html>
+
+<html>
+    <body>
+        <h1>Belajar Tag Video</h1>
+        <video controls autoplay style="widht:200px;height:250px;">
+            <source src="arab.mp4" type="video/mp4">
+        </video>
+    </body>
+</html>
+
+<html lang="en">
+    <table border="3">
+        <body>
+            <center><h1>Selamat Datang Di Profilku</h1></center>
+        <hr>
+         <body>
+           <center><img alt="Foto Saya" src="dewe1.jpg" height="200"/></center>
+         </body>
+         <hr>
+              <p>
+               <center>nama: Chaidar Ali</center>
+               <center>kelas: XI</center>
+               <center>jurusan: RPL(Rekayasa Perangkat Lunak)</center>
+              <center>tempat sekolah: <a href="link.html">SMK NEGERI 9 MALANG</a></center>
+              </p>
+         <hr>
+         <center><div>Copyright &copy; 2020 by Haidar</div></center>
+           </body>
+           </table>
+         </html>
+    </table>
+</html>
